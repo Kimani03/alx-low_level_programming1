32b1cdf15c96a587a8bf1ccc084eb6934287710a
Kimani03 <vicrorkariuki03@gmail.com>
2022-09-10 10:45:41 -0700

    Task 1. The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -29,3 +29,8 @@ int main(void)
 
 	return (0);
 }
+total 8.0K
+   0 drwxr-xr-x 2 root root  55 Sep  9 01:43 .
+   0 drwxr-xr-x 5 root root  78 Sep  9 01:39 ..
+4.0K -rwxr-xr-x 1 root root 391 Sep  9 01:42 0-positive_or_negative.c
+4.0K -rw-r--r-- 1 root root 197 Sep  9 01:43 README.md
