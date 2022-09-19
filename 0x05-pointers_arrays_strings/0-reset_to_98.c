0407c5fb558b3e4591da5be6c00d3f392864ab2e
Kimani03 <victorkariuki03@gmail.com>
2022-09-19 10:34:09 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,12 @@
-/*
- * File: 0-reset_to_98.c
- * Auth: Victor K Kimani
- */
-
 #include "main.h"
 
 /**
- * reset_to_98 - Takes a pointer to an int and updates
- *               the value it points to to 98.
- * @n: The pointer to an int.
+ * reset_to_98 - resets the value of the given integer to 98
+ * @n: parameter 
+ * Return : nothing
  */
+ 
 void reset_to_98(int *n)
 {
-	*n = 98;
+        *n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,14 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * File: main.h
- * Auth: Victor K Kimani
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
+  * void prototypes()
+  * int prototypes()
+  */
 
-#include <stdlib.h>
-
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -22,5 +17,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
