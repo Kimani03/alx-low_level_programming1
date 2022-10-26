c6629f2ce97bba598b7e0af10d9795f92b3d27f5
Kimani03 <victorkariuki03@gmail.com>
2022-10-25 23:45:21 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
-void first(void) __attribute__ ((constructor));
+#include "lists.h"
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * print_b4_main - prints string before the main function is executed
+ *
+ * Return: nothing
  */
-void first(void)
+
+void print_b4_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
